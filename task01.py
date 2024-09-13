5b6e3d38ce4c4a7409a0ebc508d406d59b49ec2a
SrothJr <nazimhossain14602@gmail.com>
2024-09-13 15:21:40 +0600

    Adding comments to task01

--- task01.py
+++ task01.py
@@ -1,21 +1,22 @@
 # Dijkstra's algorithm
 import heapq as hq
+#..Using heap queue cause it's a greedy method 
 
 def djs(src, adj):
     n = len(adj)
     pq = []
-    hq.heappush(pq, (0, src))
+    hq.heappush(pq, (0, src))  #..Pushing the source and it's weight which is zero.
 
-    distance = [float("inf")] * n
+    distance = [float("inf")] * n #...List to store distance, initially all nodes have infinity value.
     distance[src] = 0
 
     while pq:
         current_distance, u = hq.heappop(pq)
 
         for v, weight in adj[u]:
-            if distance[v] > distance[u] + weight:
+            if distance[v] > distance[u] + weight: #...If current distance is more than the distance of parent + parent to child, update it.
                 distance[v] = distance[u] + weight
-                hq.heappush(pq, (distance[v], v))
+                hq.heappush(pq, (distance[v], v))  #....Pushing the new values of weight and node to heap.
     
     
     return distance
@@ -26,7 +27,7 @@ output_file = open("output/output01.txt", "w")
 n , m = [int(i)+1 for i in input_file.readline().strip().split(" ")]
 
 edges = []
-for i in range(1, m):
+for i in range(1, m):   #....The lab stask starts from 1
     edges.append([int(i) for i in input_file.readline().strip().split(" ")])
 
 source = int(input_file.readline().strip())
